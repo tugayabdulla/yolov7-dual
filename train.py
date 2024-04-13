32258d7cb4c155482fd99fbc66005f668f42903b
tugayabdulla <abdullazadetuqay@gmail.com>
2024-04-13 13:21:52 +0200

    Freezing backbone

--- train.py
+++ train.py
@@ -120,6 +120,7 @@ def train(hyp, opt, device, tb_writer=None):
     for k, v in model.named_parameters():
         v.requires_grad = True  # train all layers
         if any(x in k for x in freeze) or "backbone" in k:
+            print('freezing %s' % k)
             v.requires_grad = False
 
     # Optimizer
