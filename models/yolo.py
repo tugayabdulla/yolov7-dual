fde2804f5972e194b3a618ce9d1deb1db90bb7a4
tugayabdulla <abdullazadetuqay@gmail.com>
2024-04-17 00:17:19 +0200

    fixed est

--- models/yolo.py
+++ models/yolo.py
@@ -904,20 +904,23 @@ class Model(nn.Module):
     def fuse(self):  # fuse model Conv2d() + BatchNorm2d() layers
         # clear
         print('Fusing layers... ')
-        for m in self.model.modules():
-            if isinstance(m, RepConv):
-                #print(f" fuse_repvgg_block")
-                m.fuse_repvgg_block()
-            elif isinstance(m, RepConv_OREPA):
-                #print(f" switch_to_deploy")
-                m.switch_to_deploy()
-            elif type(m) is Conv and hasattr(m, 'bn'):
-                m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv
-                delattr(m, 'bn')  # remove batchnorm
-                m.forward = m.fuseforward  # update forward
-            elif isinstance(m, (IDetect, IAuxDetect)):
-                m.fuse()
-                m.forward = m.fuseforward
+
+        for part in [self.model.values()]:
+
+            for m in part.modules():
+                if isinstance(m, RepConv):
+                    #print(f" fuse_repvgg_block")
+                    m.fuse_repvgg_block()
+                elif isinstance(m, RepConv_OREPA):
+                    #print(f" switch_to_deploy")
+                    m.switch_to_deploy()
+                elif type(m) is Conv and hasattr(m, 'bn'):
+                    m.conv = fuse_conv_and_bn(m.conv, m.bn)  # update conv
+                    delattr(m, 'bn')  # remove batchnorm
+                    m.forward = m.fuseforward  # update forward
+                elif isinstance(m, (IDetect, IAuxDetect)):
+                    m.fuse()
+                    m.forward = m.fuseforward
         self.info()
         return self
 
