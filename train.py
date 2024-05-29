b42ef9266b7ce617429c2fde4a952fbf4ee140e2
tugayabdulla <abdullazadetuqay@gmail.com>
2024-05-29 13:34:10 +0200

    ADD fusion

--- train.py
+++ train.py
@@ -614,7 +614,6 @@ if __name__ == '__main__':
     parser.add_argument('--freeze', nargs='+', type=int, default=[0], help='Freeze layers: backbone of yolov7=50, first3=0 1 2')
     parser.add_argument('--v5-metric', action='store_true', help='assume maximum recall as 1.0 in AP calculation')
     opt = parser.parse_args()
-
     # Set DDP variables
     opt.world_size = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1
     opt.global_rank = int(os.environ['RANK']) if 'RANK' in os.environ else -1
